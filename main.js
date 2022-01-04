f17b4c4c8ee6b84b1896bba13a9af433b87e4cdf
task58 <taskr3g@gmail.com>
2022-01-04 12:07:16 +0900

    録音ができるように変更

--- main.js
+++ main.js
@@ -6,7 +6,38 @@ const CP = require('child_process');
 const readlineSync = require('readline-sync');
 const socketio = require("socket.io")
 const yaml = require("js-yaml");
-const configChacker = require("./lib/configChecker")
+const configChacker = require("./lib/configChecker");
+const path = require("path");
+const os = require("os");
+
+const name = os.userInfo().username;
+const homeDir = os.userInfo().homedir
+const appDir = path.posix.join(homeDir,".wine","drive_c","users",name,"YukkuriVoiceW")
+
+// var appDirWinA = path.win32.join("c:","users",name,"YukkuriVoiceW");
+// var appDirWinB = appDirWinA.split("\\")
+// const app DirWin = appDirWinB.join("\\\\")
+//を1行で済ませる
+const appDirWin = path.win32.join("c:","users",name,"YukkuriVoiceW").split("\\").join("\\\\")
+
+console.log(appDirWin)
+
+try {
+    fs.accessSync(appDir)
+} catch (error) {
+   
+    if(error.code = "ENOENT"){
+        console.error("アプリケーションフォルダが存在しません")
+        console.log(appDir+"にフォルダを作成します")
+        fs.mkdirSync(appDir)
+        fs.mkdirSync(appDir+"/record")
+        console.log(homeDir+"にショートカットを作成します")
+        CP.execSync("ln -s "+appDir+" "+homeDir)
+    }else{
+        console.error(error)
+        process.exit(2)
+    }
+}
 
 const app = express();
 
@@ -27,12 +58,24 @@ app.get("/",(req,res)=>{
 console.log("このソフトではsoftalkフォルダ内のsoftalkw.exeを使用します。")
 let priset = readlineSync.question("使用するプリセット名を入力してください\n>");
 
-const command = (text)=>{
-    return `wine ${config.SofTalkDir}/softalkw.exe /PR:${priset} /X:1 /W:${text}`
+let rec = readlineSync.question("音声を録音しますか？\n録音するなら0を入力\n>");
+let recbool = false;
+if(parseInt(rec) == 0){
+    recbool = true;
 }
 
-const read =(text)=>{
-    CP.exec(command(text),(err,stdout,stderr)=>{
+let recCount = 0;
+
+const command = (text,rec)=>{
+    if(rec){
+        return `wine start ${config.SofTalkDir}/softalkw.exe /PR:${priset} /X:1 /R:${appDirWin}\\\\record\\\\rec_${recCount}_.wav /W:${text}`
+    }else{
+        return `wine start ${config.SofTalkDir}/softalkw.exe /PR:${priset} /X:1 /W:${text}`
+    }
+}
+
+const read =(text,rec)=>{
+    CP.exec(command(text,rec),(err,stdout,stderr)=>{
         if (err) {
             console.log(`err: ${stderr}`)
             process.exit(1);
@@ -46,31 +89,15 @@ var io = socketio(server);
 io.on('connection', function(socket){
     socket.on("speak",(msg)=>{
         console.log(msg)
-        read(msg)
+        read(msg,recbool)
     })
 });
 
 
 server.listen(8000,()=>{
     console.log("server start!")
-    read("プログラムを開始します");
-})
-
-// コンフィグをチェックするための関数
-
-function checkConfig(config){
-    if(!config.SofTalkDir){
-        console.error("コンフィグ SofTalkDir の値が空か、プロパティが存在していません")
-        process.exit(2);
+    if (recbool) {
+        console.log("録音モード")
     }
-    fs.access(config.SofTalkDir,(err)=>{
-        if(!err)return;
-        if (error.code === "ENOENT") {
-            console.error("コンフィグ SofTalkDir に指定されたディレクトリが存在しません")
-        } else {
-            console.error("予期せぬエラーが発生しました")
-        }
-        process.exit(2);
-    })
-}
-
+    read("プログラムを開始します",false);
+})
